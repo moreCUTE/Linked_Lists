280a5c15e885523f317db43556347bd4a0a3ca66
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2022-11-08 14:43:39 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 class Student:
-    def __init__(self, name, ID, next=None):
+    def __init__(self, name, last, ID, next=None):
         self.name = name
+        self.last = last
         self.ID = ID
         self.next = next
 #---------------------------------------------
@@ -8,8 +9,8 @@ class LinkedList:
     def __init__(self):
         self.head = None
         
-    def insert(self, name, ID):
-        newStudent = Student(name, ID)
+    def insert(self, name, last, ID):
+        newStudent = Student(name, last, ID)
         if self.head:
             current = self.head        
             while current.next:
@@ -21,8 +22,20 @@ class LinkedList:
     def PrintList(self):
         current = self.head
         while current:
-            print("student name:", current.name, ", ID:", current.ID)
+            if Student == current.name:
+                return True
+            else:
+                return False
             current = current.next
+            
+    def getListNum(self):
+        num = 0
+        current = self.head
+        while current:
+            num+=1
+            current = current.next
+        print("there are", num, "objects in the linked list")
+            
             
     def TakeNumber(self):
         print("enter student name: ")
@@ -35,15 +48,19 @@ class LinkedList:
             else:
                 num += 1
                 current = current.next
+                
             
+    #def List
 #-----------------------------------------------
 StudentLine = LinkedList()
-StudentLine.insert("Lily", 456987)
-StudentLine.insert("Kevin", 123987)
-StudentLine.insert("Sebastian", 4)
-StudentLine.insert("Brandon", 456987)
-StudentLine.insert("Eli", 123987)
-StudentLine.insert("Jacob", 4)
+StudentLine.insert("Lily", "lasname", 456987)
+StudentLine.insert("Kevin", "Salasar", 123987)
+StudentLine.insert("Sebastian", "Hernandez", 987456)
+StudentLine.insert("Brandon", "Sandoval", 321890)
+StudentLine.insert("Eli", "Earlix", 654789)
+StudentLine.insert("Kyle", "Kubaska", 796805)
+
 
 StudentLine.PrintList()
+StudentLine.getListNum()
 print(StudentLine.TakeNumber())
